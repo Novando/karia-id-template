0b9df0a705acf965b5b226006c65c89068023884
Rizky Novando Priyadi <rizkynovando@gmail.com>
2024-07-23 01:32:46 +0700

    feature: usp and partner

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,36 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom-xl': '0 3px 40px 0 rgb(0 0 0 / 0.1)'
+      },
       colors: {
         primary: {
           DEFAULT: 'var(--primary)',
           hover: 'var(--primary-hover)',
           fg: 'var(--primary-fg)',
           fgh: 'var(--primary-fg-hover)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          hover: 'var(--secondary-hover)',
+          fg: 'var(--secondary-fg)',
+          fgh: 'var(--secondary-fg-hover)',
+        },
+        ternary: {
+          DEFAULT: 'var(--ternary)',
+          hover: 'var(--ternary-hover)',
+          fg: 'var(--ternary-fg)',
+          fgh: 'var(--ternary-fg-hover)',
+        },
+        background: {
+          DEFAULT: 'var(--background)',
+        },
+        foreground: {
+          DEFAULT: 'var(--foreground)',
         },
       },
     },
